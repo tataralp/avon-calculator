2bb29c8e3bcce83ddf4681500a382b35a372cf61
tatarani <tataralp@gmail.com>
2016-04-18 15:18:54 +0200

    init commit

---
+++ src/main/java/hu/inf/unideb/avon_calculator/App.java
@@ -0,0 +1,13 @@
+package hu.inf.unideb.avon_calculator;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
